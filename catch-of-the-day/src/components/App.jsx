5f0a03b9e22aea352005b2ac2dbe0cad17bb9305
robbyhecht <robbyhecht@gmail.com>
2019-06-13 15:32:18 -0500

    through ch14

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -4,6 +4,20 @@ import Inventory from './Inventory'
 import Order from './Order'
 
 class App extends React.Component {
+
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    // *
+    addFish = fish => {
+        // **
+        const fishes = { ...this.state.fishes }; // ... is an object spread
+        fishes[`fish${Date.now()}`] = fish; // Date.now provides a unique identifier
+        this.setState({ fishes }); // fishes:fishes is unnecesary in ES6
+    };
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -11,10 +25,18 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish}/>
             </div>
         )
     }
 }
 
 export default App;
+
+// * methods that update state must live in the same component as the state they are updating
+
+// ** steps for updating state:
+//  1. Take a copy of the existing state (don't want to modify state directly- this would be a mutation)
+ // 2. Add out new fish to the fishes variable
+ // 3. Set the new fishes object to state
+
